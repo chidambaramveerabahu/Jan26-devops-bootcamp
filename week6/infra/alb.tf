f23c4eefe6ccaf4eddfd658ce6aa8693b0c7d3cc
cv mahesh <cvmahesh@localhost.localdomain>
2026-03-31 22:45:47 +0530

    fix: comment out route53/acm/https for now

--- week6/infra/alb.tf
+++ week6/infra/alb.tf
@@ -36,15 +36,15 @@ resource "aws_lb_listener" "app_listener" {
   }
 }
 
-# https listener
-resource "aws_lb_listener" "app_https_listener" {
-  load_balancer_arn = aws_lb.app_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.app_cert.arn
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
+# # https listener
+# resource "aws_lb_listener" "app_https_listener" {
+#   load_balancer_arn = aws_lb.app_alb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = aws_acm_certificate.app_cert.arn
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app_tg.arn
+#   }
+# }
